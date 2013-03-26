f6808d09d75951b3419c999e03d899ec930bbe44
Tom Erik Støwer <testower@gmail.com>
2013-03-26 22:28:32 +0100

    Initial Commit

---
+++ Legevakt/TESAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TESAppDelegate.h
+//  Legevakt
+//
+//  Created by Tom Erik Støwer on 3/26/13.
+//  Copyright (c) 2013 Tom Erik Støwer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Legevakt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Legevakt
+//
+//  Created by Tom Erik Støwer on 3/26/13.
+//  Copyright (c) 2013 Tom Erik Støwer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TESAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TESAppDelegate class]));
+    }
+}
